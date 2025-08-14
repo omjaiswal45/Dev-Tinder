79ea646027e3d11f951aa4ee31662e5da4a45a0e
omjaiswal45 <omjai11022000@gmail.com>
2025-08-15 00:50:18 +0530

    signup

--- src/app.js
+++ src/app.js
@@ -1,40 +1,41 @@
 const express = require("express")
 
 const app = express();
-
-const{adminAuth, userAuth} = require("./middlewares/auth");
-
-app.use("/admin", adminAuth);
-
-app.get("/admin/accessdata", (req, res) =>{
-  //check the authorization using middleware
-  res.send("all data sent")
+const User = require("./models/user")
+const connectDB = require("./config/database")
+app.use(express.json());
+app.post('/signup', async(req, res)=>{
+  console.log(req.body)
+  // creating instaances of the User model req.body return js obj
+const user = new User(req.body);
+try {
+  await user.save();
+  res.send("user is crreated successfully")
+} catch (err) {
+  res.status(400).send("error saving the user:" + err.message);
+  
+}
 })
 
-app.post("/admin/adddata", (req, res) =>{
-  //check the authorization here allso using middleware
-  res.send("items is add to DB")
-})
- app.delete("/admin/delete", (req, res) =>{
-  // here also check authorization
-  res.send("items deleted")
- })
+//feed api
 
- // here all the /user routerhandler 
-app.post("/user/signup", (req,res)=>{
-  res.send("user successful signup")
+app.get("/feed", async(req,res) =>{
+  try {
+    const alluser = await User.find({});
+    res.send(alluser)
+    
+  } catch (err) {
+    res.status(400).send("somethings went wrong");
+  }
 })
 
-app.get("/user/login", userAuth, (req, res) =>{
-  //here chekc auth
-  res.send("user successfully login ")
-})
-
- app.get("/user/profile", userAuth, (req,res) =>{
-  // check user authontication 
-  res.send ("here the userprofile")
- })
-
-app.listen(4000, () =>{
+connectDB().then(() =>{
+  console.log("db conn established....")
+  app.listen(4000, () =>{
   console.log("server is running at http://localhost:4000/")
 });
+})
+.catch((err) =>{
+  console.log("db is not be connected")
+})
+
